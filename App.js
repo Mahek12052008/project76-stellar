9cfaf3e6196784dbd1bd15674b258d221d92a4ef
Mahek12052008 <83763128+Mahek12052008@users.noreply.github.com>
2021-09-17 19:29:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from 'react-navigation-stack'
+import { NavigationContainer } from 'react-navigation';
+import HomeScreen from './screens/HomeScreen';
+import DailyPic from './screens/DailyPic';
+import SpaceCraft from './screens/SpaceCraft';
+import StarMap from './screens/StarMap';
+
+const Stack = createStackNavigator();
+
+export default class App extends React.Component() {
+  render(){
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initiaRouteName='Home' screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Home' component={HomeScreen}/>
+        <Stack.Screen name='DailyPic' component={DailyPic}/>
+        <Stack.Screen name='SpaceCraft' component={SpaceCraft}/>
+        <Stack.Screen name='StarMap' component={StarMap}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+  }
+}
---
+++ screens/SpaceCraft.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class SpaceCraft extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>space craft</Text>
+            </View>
+        )
+    }
+}
